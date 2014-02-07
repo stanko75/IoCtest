a0eb2ddacfc968e6b27bfdc8fbfae5f03ea099a1
stanko75 <stanko@milosev.com>
2014-02-07 09:56:05 +0100

    UI

---
+++ IoCtest/IoC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoCtest
+{
+  public interface IOutput
+  {
+    void Write(string content);
+  }
+
+  public class MemoOutput : IOutput
+  {
+
+    public void Write(string content)
+    {
+      //write to memo
+      throw new NotImplementedException();
+    }
+  }
+
+  public class ConsoleOutput : IOutput
+  {
+    public void Write(string content)
+    {
+      //write to console
+      throw new NotImplementedException();
+    }
+  }
+
+  class IoC
+  {
+
+  }
+}
