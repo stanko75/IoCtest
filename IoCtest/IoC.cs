693c84396d434f308ad2e7352f8b8c605cdaeb69
stanko75 <stanko@milosev.com>
2014-02-09 00:45:32 +0100

    Revert "Git GUI"
    This reverts commit 2d69899e2952b30373fb42bc0ea6ef04ed3a7376.

--- IoCtest/IoC.cs
+++ IoCtest/IoC.cs
@@ -12,29 +12,32 @@ namespace IoCtest
     void Write(string content);
   }
 
-  public class TextBoxOutput : IOutput
-  {
-
-    public TextBox _textBox;
-
-    public void Write(string content)
-    {
-      //write to memo
-      _textBox.Text = content;
-    }
-  }
-
   public class ConsoleOutput : IOutput
   {
     public void Write(string content)
     {
-      //write to console
       Console.WriteLine(content);
     }
   }
 
-  class IoC
+  public interface IDateWriter
   {
+    void WriteDate(object obj);
+  }
 
+  public class TodayWriter : IDateWriter
+  {
+    private IOutput _output;
+    public TodayWriter(IOutput output)
+    {
+      this._output = output;
+    }
+
+    public void WriteDate(object obj)
+    {
+      var ioCmessageDisplayTextBox = (TextBox)obj;
+      ioCmessageDisplayTextBox.Text = "Hello world!";
+      this._output.Write(DateTime.Today.ToShortDateString());
+    }
   }
 }
